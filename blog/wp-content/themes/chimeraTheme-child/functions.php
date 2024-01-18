ecb86654a087f48ed7f9a6547cf91ec41a8f6340
Brittany Kelly <bkelly@resulta.com>
2024-01-18 17:03:39 -0400

    bkup changes 2

--- blog/wp-content/themes/chimeraTheme-child/functions.php
+++ blog/wp-content/themes/chimeraTheme-child/functions.php
@@ -1,4 +1,10 @@
 <?php
+// Enable WP_DEBUG mode
+// define( 'WP_DEBUG', true );
+
+// Enable Debug logging to the /wp-content/debug.log file
+// define( 'WP_DEBUG_LOG', true );
+
 	//* Code goes here woop woop */ 
 add_action('wp_enqueue_scripts', 'enqueue_parent_styles');
 
