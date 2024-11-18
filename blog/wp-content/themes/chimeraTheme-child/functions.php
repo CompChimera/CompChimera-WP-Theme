4b630759c98dad684ddb1ac112ab5adccf8720ff
Brittany Kelly <computationalchimera@gmail.com>
2024-11-18 12:24:20 -0400

    Security additions

--- blog/wp-content/themes/chimeraTheme-child/functions.php
+++ blog/wp-content/themes/chimeraTheme-child/functions.php
@@ -6,3 +6,14 @@ function enqueue_parent_styles(){
 	wp_enqueue_style('parent-style', get_template_directory_uri().'/style.css');
 
 }
+
+function cc_hide_wp_version() {
+    return '';
+}
+add_filter('the_generator', 'cc_hide_wp_version');
+
+function cc_login_error(){
+	return "Error: Incorrect login.";
+}
+
+add_filter('login_errors', 'cc_login_error');
